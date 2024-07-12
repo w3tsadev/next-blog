8ba27eec7269e9b321cddf9dbb31fae572e573fe
Tanveer Sayem <tanveersayem@gmail.com>
2024-07-11 22:39:00 -0400

    updated the fetchURL

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,4 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const fetchUrl = "http://localhost:3000/api";
+export const fetchUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api"
+    : "https://next-blog-cj.vercel.app/";
